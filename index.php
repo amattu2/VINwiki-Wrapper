47edf02cd0c2873e2f79e69e3595c23c16264682
Alec M <alecmattu1234@gmail.com>
2021-03-31 18:40:42 -0400

    Add fetch_feed demo

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
-use amattu;
-
+// Setup wrapper
 require("classes/vinwiki.class.php");
 
-$Wrapper = new amattu\VINWiki("", "");
+// Spawn a new instance
+$wrapper = new amattu\VINWiki();
+
+// Fetch a vehicle feed
+echo "<pre>";
+print_r($wrapper->fetch_feed("4JGBB8GB4BA662410"));
+echo "</pre>";
 ?>
