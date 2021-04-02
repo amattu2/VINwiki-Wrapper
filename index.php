04f728e4cc9d5894b70ebb80f27de22c3de4f18b
Alec M <alecmattu1234@gmail.com>
2021-04-02 15:03:52 -0400

    Comment out functions by default

--- index.php
+++ index.php
@@ -7,25 +7,33 @@ $wrapper = new amattu\VINWiki();
 
 // Setup a Vinwiki session
 // Only required for create/update functions
-$wrapper->setup_session("user", "pass");
+$wrapper->setup_session("USERNAME", "PASSWORD");
 
+/*
 // Fetch a vehicle feed
 echo "<h1>Fetch Feed By VIN</h1>", "<pre>";
 print_r($wrapper->fetch_feed("4JGBB8GB4BA662410"));
 echo "</pre>";
+*/
 
+/*
 // Fetch vehicle decode by license plate (pl82vin)
 echo "<h1>Plate 2 VIN Decode</h1>", "<pre>";
 print_r($wrapper->pl82vin("HTML", "MD"));
 echo "</pre>";
+*/
 
+/*
 // Update Vehicle By VIN
 echo "<h1>Update Vehicle</h1>", "<pre>";
 print_r($wrapper->update_vehicle("WBAWB735X9P159047", 2009, "BMW", "335i", "M Sport"));
 echo "</pre>";
+*/
 
+/*
 // Create a post on a VIN
 echo "<h1>Create a feed post</h1>", "<pre>";
 print_r($wrapper->create_post("WBAWB735X9P159047", Array("text" => "added to vinwiki via API")));
 echo "</pre>";
+*/
 ?>
